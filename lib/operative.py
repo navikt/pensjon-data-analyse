b8f57b3f4a93e28aeac5b5c4c0a2a20f5a110c38
vebjorre <vebjorn.rekkebo@nav.no>
2022-05-05 08:54:33 +0000

    Update

--- lib/operative.py
+++ lib/operative.py
@@ -6,10 +6,19 @@ import plotly.graph_objects as go
 from typing import Dict
 
 names = ["Nullpunktsmåling", "Måling T1", "Måling T2", "Måling T3"]
+categorical_colors = (
+    "#0067C5",
+    "#BA3A26",
+    "#06893A",
+    "#634689",
+    "#FF9100",
+    "#66CBEC",
+    "#F0C419",
+)
 
-def make_fig(col: pd.core.series.Series):
+def make_fig(col: pd.core.series.Series, n: int):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=col.index[[3,4,6,8]], y=col[names].values, marker=dict( size = [10 for i in range(3)] )))
+    fig.add_trace(go.Scatter(x=col.index[[3,4,6,8]], y=col[names].values, marker=dict(size=10, color=categorical_colors[n])))
     fig.update_xaxes(title=None)
     if col.prosent_flagg:
         fig.update_yaxes(title=None, range=[0,1], tickformat='.0%')
@@ -35,9 +44,9 @@ class MP:
         
 
     @classmethod
-    def from_series(cls, col: pd.core.series.Series):
+    def from_series(cls, col: pd.core.series.Series, n: int):
         return MP(col.Måleparameter,
-                  make_fig(col),
+                  make_fig(col, n),
                   col)
 
 @dataclass
@@ -48,7 +57,6 @@ class Operative:
     def __init__(self, name: str):
         self.name = name
         self.MPs = {}
-        self.nMPS = 0
     
     def show_figs(self):
         for _, mp in self.MPs.items():

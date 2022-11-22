fb32bed838e410dd6d545bf6f288feed0f7c5049
vebjorre <vebjorn.rekkebo@nav.no>
2022-11-22 14:41:59 +0000

    look at ledetid

--- lib/pandas_utils.py
+++ lib/pandas_utils.py
@@ -1,10 +1,35 @@
 import pandas as pd
+from time import time
 
 
-def pandas_from_sql(filename: str, con):
-    with open(filename, 'r') as file:
-        query = file.read()
-    return pd.read_sql(query, con=con)
+# def pandas_from_sql(filename: str, con):
+#     with open(filename, 'r') as file:
+#         query = file.read()
+#     return pd.read_sql(query, con=con)
+
+
+def pandas_from_sql(sqlfile, con, tuning=None):
+    with con.cursor() as cursor:
+        start = time()
+        
+        if tuning:
+            cursor.prefetchrows = tuning
+            cursor.arraysize = tuning
+        
+        with open(sqlfile) as sql:
+            cursor.execute(sql.read())
+
+        df = pd.DataFrame(cursor.fetchall())
+        
+        end = time()
+        
+        print(f'{len(df)} rad(er) ble returnert etter {end-start} sekunder.')
+        
+        if len(df) > 0:
+            df.columns = [x[0] for x in cursor.description]
+        
+        return df
+
 
 
 def add_zero_to_mnd(x: str):

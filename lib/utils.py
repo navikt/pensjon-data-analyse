38706f30b6ae15504c9ea3867e46299e91c1b532
vebjorre <vebjorn.rekkebo@nav.no>
2023-06-13 10:35:18 +0000

    WIP første DAG

--- lib/utils.py
+++ lib/utils.py
@@ -3,6 +3,17 @@ import os
 import random
 
 
+nav_colors = (
+    "#0067C5",
+    "#BA3A26",
+    "#06893A",
+    "#634689",
+    "#FF9100",
+    "#66CBEC",
+    "#F0C419",
+)
+
+
 def set_secrets_as_env(split_on=':'):
     secrets = secretmanager.SecretManagerServiceClient()
     resource_name = f"projects/knada-gcp/secrets/vebjorn-rekkebo-bac1/versions/latest"
@@ -18,4 +29,3 @@ def randomize_zeros(x):
         return random.randint(0,5)
     else:
         return x
-    
---
+++ scheduling/airflow/dags/datastory-automatisering-v0.py
@@ -0,0 +1,35 @@
+#from airflow import DAG
+
+#from airflow.utils.dates import days_ago
+#from airflow.operators.python_operator import PythonOperator
+#from kubernetes import client as k8s
+#from datetime import datetime
+#import os
+import sys
+sys.path.append('../../../scripts')
+sys.path.append('../../../lib')
+
+
+from datastory_automatisering import update_datastory
+
+def run_update_datastory():
+    update_datastory()
+    
+
+with DAG('datastory-automatisering-v0', start_date=datetime(2023, 6, 1), schedule_interval="0 0 1 * *") as dag:    
+    run_this = PythonOperator(
+    task_id='datastory-automatising',
+    python_callable=run_update_datastory,
+    executor_config={
+        "pod_override": k8s.V1Pod(
+            spec=k8s.V1PodSpec(
+                containers=[
+                k8s.V1Container(
+                    name="base",
+                    image="europe-west1-docker.pkg.dev/knada-gcp/knada/airflow-papermill:2023-03-22-fb1c4a4"
+                )
+                ]
+            )
+        )
+    },
+    dag=dag)

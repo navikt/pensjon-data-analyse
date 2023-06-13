2b8eecbedf0130e3d69581abc93a65240ce87022
vebjorre <vebjorn.rekkebo@nav.no>
2023-06-13 13:31:01 +0200

    Add allowlist

--- scheduling/airflow/dags/datastory-automatisering-v0.py
+++ scheduling/airflow/dags/datastory-automatisering-v0.py
@@ -18,18 +18,20 @@ def run_update_datastory():
 
 with DAG('datastory-automatisering-v0', start_date=datetime(2023, 6, 1), schedule_interval="0 0 1 * *") as dag:    
     run_this = PythonOperator(
-    task_id='datastory-automatising',
-    python_callable=run_update_datastory,
-    executor_config={
-        "pod_override": k8s.V1Pod(
-            spec=k8s.V1PodSpec(
-                containers=[
-                k8s.V1Container(
-                    name="base",
-                    image="ghcr.io/navikt/airflow-pensjon-sb:v0"
-                )
-                ]
+        task_id='datastory-automatising',
+        python_callable=run_update_datastory,
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    containers=[
+                    k8s.V1Container(
+                        name="base",
+                        image="ghcr.io/navikt/airflow-pensjon-sb:v0"
+                    )
+                    ]
+                ),
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "dm08db03.adeo.no:1521,data.intern.nav.no"})
             )
-        )
-    },
+        },
     dag=dag)
+    

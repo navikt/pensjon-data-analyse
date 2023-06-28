50c5f33c21405f02c8bf151c5ef6ce4e78a7ea07
vebjorre <vebjorn.rekkebo@nav.no>
2023-06-28 14:04:10 +0200

    DAG for kontrollpunkt dataproduct

---
+++ scheduling/airflow/dags/dataproduct-kontrollpunkt.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+from kubernetes import client as k8s
+from datetime import datetime
+
+
+def run_overwrite_dataproduct():
+    from scripts.dataproduct_kontrollpunkt import update_dataproduct
+    
+    overwrite_dataproduct()
+    
+
+with DAG('dataproduct-kontrollpunkt', start_date=datetime(2023, 6, 28), schedule_interval="10 06 * * *") as dag:    
+    run_this = PythonOperator(
+        task_id='dataproduct-kontrollpunkt',
+        python_callable=run_overwrite_dataproduct,
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                spec=k8s.V1PodSpec(
+                    containers=[
+                    k8s.V1Container(
+                        name="base",
+                        image="ghcr.io/navikt/airflow-pensjon-sb:v0",
+                        working_dir="/dags/scripts",
+                    )
+                    ]
+                ),
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "dm08db03.adeo.no:1521,data.intern.nav.no"})
+            )
+        },
+    dag=dag)

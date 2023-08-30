d64c6d98fd948fe7b10104b3cc3fe4a14cdfd2fe
vebjorre <vebjorn.rekkebo@nav.no>
2023-08-30 13:06:47 +0000

    more detective work

--- sql/krav_med_kontrollpunkt.sql
+++ sql/krav_med_kontrollpunkt.sql
@@ -1,6 +1,6 @@
 select
-    extract(month from kp.dato_opprettet) as mnd,
-    extract(month from kp.dato_opprettet) as yr,
+    extract(month from kh.dato_opprettet) as mnd,
+    extract(year from kh.dato_opprettet) as yr,
     dkh.dekode as kravtype,
     (case when substr(kh.opprettet_av,1,1) in ('0','1','2','3','4','5','6','7','8','9') then 'Bruker' else
     case when substr(kh.opprettet_av,1,1) not in ('0','1','2','3','4','5','6','7','8','9') and substr(kh.opprettet_av,2,1) in ('0','1','2','3','4','5','6','7','8','9') then 'Saksbehandler' else
@@ -12,25 +12,24 @@ select
     dkp.dekode_tekst as kontrollpunkt_forklaring,
     count(*) as antall
 
-from pen.t_kontrollpunkt kp
-inner join pen.t_kravhode kh on kh.kravhode_id = kp.kravhode_id
-inner join pen.t_sak s on s.sak_id = kp.sak_id
-inner join pen.t_k_krav_gjelder dkh on dkh.k_krav_gjelder = kh.k_krav_gjelder
-inner join pen.t_k_sak_t ds on ds.k_sak_t = s.k_sak_t
-inner join pen.t_k_kontrollpnkt_t dkp on dkp.k_kontrollpnkt_t = kp.k_kontrollpnkt_t
-inner join pen.t_k_krav_s dks on dks.k_krav_s = kh.k_krav_s
-inner join pen.t_k_behandling_t dkb on dkb.k_behandling_t = kh.k_behandling_t
+from pen.t_kravhode kh
+    inner join pen.t_k_krav_gjelder dkh on dkh.k_krav_gjelder = kh.k_krav_gjelder
+    inner join pen.t_k_behandling_t dkb on dkb.k_behandling_t = kh.k_behandling_t
+    inner join pen.t_k_krav_s dks on dks.k_krav_s = kh.k_krav_s
+inner join pen.t_sak s on s.sak_id = kh.sak_id
+    inner join pen.t_k_sak_t ds on ds.k_sak_t = s.k_sak_t
+left join pen.t_kontrollpunkt kp on kp.kravhode_id = kh.kravhode_id
+    left join pen.t_k_kontrollpnkt_t dkp on dkp.k_kontrollpnkt_t = kp.k_kontrollpnkt_t
 
 where s.k_sak_t = 'ALDER'
 --and kh.k_behandling_t = 'DEL_AUTO'
 --and kp.dato_opprettet < trunc(current_date, 'MM')
-
 and kh.k_krav_gjelder in ('FORSTEG_BH', 'F_BH_MED_UTL')
 and kh.k_krav_s != 'AVBRUTT'
 
 group by
-    extract(month from kp.dato_opprettet),
-    extract(month from kp.dato_opprettet),
+    extract(month from kh.dato_opprettet),
+    extract(year from kh.dato_opprettet),
     dkh.dekode,
     (case when substr(kh.opprettet_av,1,1) in ('0','1','2','3','4','5','6','7','8','9') then 'Bruker' else
     case when substr(kh.opprettet_av,1,1) not in ('0','1','2','3','4','5','6','7','8','9') and substr(kh.opprettet_av,2,1) in ('0','1','2','3','4','5','6','7','8','9') then 'Saksbehandler' else

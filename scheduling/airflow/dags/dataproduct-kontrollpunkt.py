f1cdd140d84589627a8fb72340db400cf0a308ac
vebjorre <vebjorn.rekkebo@nav.no>
2023-10-18 08:47:13 +0000

    import create_pod_operator

--- scheduling/airflow/dags/dataproduct-kontrollpunkt.py
+++ scheduling/airflow/dags/dataproduct-kontrollpunkt.py
@@ -3,6 +3,8 @@ from airflow import DAG
 from kubernetes import client as k8s
 from datetime import datetime
 
+from common.podop_factory import create_pod_operator
+
 
 with DAG('dataproduct-kontrollpunkt', start_date=datetime(2023, 6, 28), schedule_interval="10 06 * * *", catchup=False) as dag:
     t1 = create_pod_operator(

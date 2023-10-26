e21a877160afcd3dcd8681057498a04801754bfa
vebjorre <vebjorn.rekkebo@nav.no>
2023-10-26 09:44:53 +0000

    new host for pen

--- sql/anvendt_vurdering.sql
+++ sql/anvendt_vurdering.sql
@@ -1,10 +1,20 @@
-select d_vv.dekode as anvendt_vurdering, extract(year from vv.dato_opprettet) as aar, TO_CHAR(vv.dato_opprettet, 'yyyy-mm') as aar_maaned, count(*) antall
+select 
+    d_vv.dekode as anvendt_vurdering, 
+    extract(year from vv.dato_opprettet) as aar, 
+    TO_CHAR(vv.dato_opprettet, 'yyyy-mm') as aar_maaned, 
+    d_kh.dekode as kravtype,
+    count(*) as antall
 from pen.t_vilkar_vedtak vv
-    inner join pen.t_k_vilk_vurd_t d_vv on d_vv.k_vilk_vurd_t = vv.k_vilk_vurd_t
+    left join pen.t_k_vilk_vurd_t d_vv on d_vv.k_vilk_vurd_t = vv.k_vilk_vurd_t
     inner join pen.t_vedtak v on v.vedtak_id = vv.vedtak_id
     inner join pen.t_kravhode kh on kh.kravhode_id = v.kravhode_id
+    inner join pen.t_k_krav_gjelder d_kh on d_kh.k_krav_gjelder = kh.k_krav_gjelder
     inner join pen.t_sak s on s.sak_id = v.sak_id
-where s.k_sak_t = 'ALDER'
-    and kh.k_krav_gjelder in ('F_BH_BO_UTL','F_BH_MED_UTL','FORSTEG_BH','F_BH_KUN_UTL')
-group by extract(year from vv.dato_opprettet), TO_CHAR(vv.dato_opprettet, 'yyyy-mm'), d_vv.dekode
+where s.k_sak_t = 'GJENLEV'
+    --and kh.k_krav_gjelder in ('F_BH_BO_UTL','F_BH_MED_UTL','FORSTEG_BH','F_BH_KUN_UTL')
+group by 
+    extract(year from vv.dato_opprettet), 
+    TO_CHAR(vv.dato_opprettet, 'yyyy-mm'),
+    d_vv.dekode, 
+    d_kh.dekode
 order by aar_maaned desc, antall desc

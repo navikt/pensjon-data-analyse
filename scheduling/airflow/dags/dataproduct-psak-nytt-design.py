b8cf7b81c22c8e996ef45d40b678dd9e6c27e3d0
vebjorre <vebjorn.rekkebo@nav.no>
2023-11-22 08:54:16 +0000

    test dataverk

--- scheduling/airflow/dags/dataproduct-psak-nytt-design.py
+++ scheduling/airflow/dags/dataproduct-psak-nytt-design.py
@@ -1,17 +1,18 @@
 from airflow import DAG
-from datetime import datetime
+from airflow.utils.dates import days_ago
+from dataverk_airflow import python_operator
 
-from common.podop_factory import create_pod_operator
+#from common.podop_factory import create_pod_operator
 
 
-with DAG('dataproduct-psak-nytt-design', start_date=datetime(2023, 6, 29), schedule_interval="15 */1 * * *", catchup=False) as dag:
+with DAG('dataproduct-psak-nytt-design', start_date=days_ago(1), schedule_interval="15 */1 * * *", catchup=False) as dag:
     t1 = create_pod_operator(
         dag=dag,
         name="dataproduct-psak-nytt-design",
         slack_channel="#pensak-airflow-alerts",
         repo="navikt/pensjon-data-analyse",
         script_path="scripts/dataproduct_psak_nytt_design.py",
-        image="ghcr.io/navikt/pensak-airflow-images:2023-09-08-531834a-main",
+        requirements_path="requirements.txt",
         branch="main",
         retries=0
     )

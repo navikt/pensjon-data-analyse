d9d7e38e87f4cfbf3221f6cdbb94a6a93c68d83b
vebjorre <vebjorn.rekkebo@nav.no>
2024-01-23 13:05:45 +0100

    revert 'switch to original host for pen'

--- lib/pesys_utils.py
+++ lib/pesys_utils.py
@@ -36,8 +36,8 @@ fra_bruker = {"Førstegangsbehandling", # Ta bort aldersovergang?
 
 
 def open_pen_connection():
-    ORACLE_HOST = 'dm08db03.adeo.no' # Original host
-    # ORACLE_HOST = 'a01dbfl041.adeo.no' # Temporary host
+    # ORACLE_HOST = 'dm08db03.adeo.no' # Original host
+    ORACLE_HOST = 'a01dbfl041.adeo.no' # Temporary host
     ORACLE_PORT = '1521'
     ORACLE_SERVICE = 'pen'
     dsnStr = cx_Oracle.makedsn(ORACLE_HOST, ORACLE_PORT, service_name=ORACLE_SERVICE)

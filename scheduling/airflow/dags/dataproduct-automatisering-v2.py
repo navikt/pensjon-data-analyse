38083dee7e91197e14e63a946de06fa5c7752b4d
vebjorre <vebjorn.rekkebo@nav.no>
2024-03-01 11:19:25 +0100

    tune requests and set earlier start_date

--- scheduling/airflow/dags/dataproduct-automatisering-v2.py
+++ scheduling/airflow/dags/dataproduct-automatisering-v2.py
@@ -4,7 +4,7 @@ from airflow.utils.dates import days_ago
 from dataverk_airflow import python_operator
 
 
-with DAG('dataproduct-automatisering-v2', start_date=days_ago(1), schedule_interval="22 4 1 * *", catchup=True) as dag:
+with DAG('dataproduct-automatisering-v2', start_date=days_ago(40), schedule_interval="22 4 1 * *", catchup=True) as dag:
     t1 = python_operator(
         dag=dag,
         name="dataproduct-automatisering-v2",
@@ -15,7 +15,7 @@ with DAG('dataproduct-automatisering-v2', start_date=days_ago(1), schedule_inter
         branch="main",
         retries=0,
         resources=k8s.V1ResourceRequirements(
-            requests={"memory": "10Gi", "cpu": "0.5", "ephemeral-storage": "5Gi"}
+            requests={"memory": "6Gi", "cpu": "1", "ephemeral-storage": "200Mi"},
             ),
         allowlist=["secretmanager.googleapis.com", "bigquery.googleapis.com", "dm08db03-vip.adeo.no:1521"]
     )

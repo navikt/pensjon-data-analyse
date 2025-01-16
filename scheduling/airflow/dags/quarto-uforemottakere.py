979574975bc137e130afc6c5d689fab6c82086b6
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-01-16 10:46:53 +0100

    fikser feil cron-intervall og legger til uv pip install

--- scheduling/airflow/dags/quarto-uforemottakere.py
+++ scheduling/airflow/dags/quarto-uforemottakere.py
@@ -6,13 +6,14 @@ from dataverk_airflow import quarto_operator
 
 with DAG(
     dag_id="quarto-uforemottakere",
-    schedule_interval="6 30 * * *",
+    schedule_interval="30 6 * * 1",
     start_date=datetime(2025, 1, 16, tzinfo=timezone("Europe/Oslo")),
     doc_md="Datafortelling med månedlige uføremottakere fra dvhi.",
     catchup=False,
 ) as dag:
     update_quarto = quarto_operator(
         dag=dag,
+        retries=0,
         name="update-quarto-uforemottakere",
         repo="navikt/pensjon-data-analyse",
         quarto={
@@ -21,10 +22,10 @@ with DAG(
             "id": "1168253f-0bfb-4a8c-964c-e1796582f72c",
             "token": Variable.get("PENSAK_QUARTO_TOKEN"),
         },
-        requirements_path="scheduling/airflow/docker/requirements_oracle.txt",
-        slack_channel="#pensak-airflow-alerts",
-        retries=0,
+        use_uv_pip_install=True,
         allowlist=["dvh.adeo.no"],
+        slack_channel="#pensak-airflow-alerts",
+        requirements_path="scheduling/airflow/docker/requirements_oracle.txt",
     )
 
     update_quarto

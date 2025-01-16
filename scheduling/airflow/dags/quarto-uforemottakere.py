4ba72c5372c70ec419106e0307fbe56755bd74fb
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-01-16 10:41:34 +0100

    DAG for kjøring av uføremottakere.qmd

---
+++ scheduling/airflow/dags/quarto-uforemottakere.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from datetime import datetime
+from pendulum import timezone
+from airflow.models import Variable
+from dataverk_airflow import quarto_operator
+
+with DAG(
+    dag_id="quarto-uforemottakere",
+    schedule_interval="6 30 * * *",
+    start_date=datetime(2025, 1, 16, tzinfo=timezone("Europe/Oslo")),
+    doc_md="Datafortelling med månedlige uføremottakere fra dvhi.",
+    catchup=False,
+) as dag:
+    update_quarto = quarto_operator(
+        dag=dag,
+        name="update-quarto-uforemottakere",
+        repo="navikt/pensjon-data-analyse",
+        quarto={
+            "path": "quarto/uforemottakere.qmd",
+            "env": "prod",
+            "id": "1168253f-0bfb-4a8c-964c-e1796582f72c",
+            "token": Variable.get("PENSAK_QUARTO_TOKEN"),
+        },
+        requirements_path="scheduling/airflow/docker/requirements_oracle.txt",
+        slack_channel="#pensak-airflow-alerts",
+        retries=0,
+        allowlist=["dvh.adeo.no"],
+    )
+
+    update_quarto

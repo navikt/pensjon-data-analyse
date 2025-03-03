5c9c14acdf019df56055671dac07d6c5cead1a50
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-03-03 11:50:10 +0100

    bytter fra lesekopi til prod

--- scheduling/airflow/dags/dataproduct-kontrollpunkt.py
+++ scheduling/airflow/dags/dataproduct-kontrollpunkt.py
@@ -8,7 +8,7 @@ with DAG('dataproduct-kontrollpunkt', start_date=days_ago(1), schedule_interval=
     t1 = python_operator(
         dag=dag,
         name="dataproduct-kontrollpunkt",
-        slack_channel="#pensak-airflow-alerts",
+        # slack_channel="#pensak-airflow-alerts",
         repo="navikt/pensjon-data-analyse",
         script_path="scripts/dataproduct_kontrollpunkt.py",
         requirements_path="requirements.txt",
@@ -17,5 +17,5 @@ with DAG('dataproduct-kontrollpunkt', start_date=days_ago(1), schedule_interval=
         resources=k8s.V1ResourceRequirements(
             requests={"memory": "2Gi", "cpu": "0.5", "ephemeral-storage": "1Gi"}
             ),
-        allowlist=["secretmanager.googleapis.com", "bigquery.googleapis.com", "dm08db03-vip.adeo.no:1521"]
+        allowlist=["secretmanager.googleapis.com", "bigquery.googleapis.com", "dm09-scan.adeo.no"]
     )

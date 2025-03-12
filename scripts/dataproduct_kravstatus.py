8604864afa5170f57ba99975b2aa91be8b03de95
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-03-12 18:32:33 +0100

    bytter fra funksjoner til flat python-fil, da denne er så kort

--- scripts/dataproduct_kravstatus.py
+++ scripts/dataproduct_kravstatus.py
@@ -9,39 +9,26 @@ logging.basicConfig(level=logging.INFO)
 pesys_utils.set_pen_secrets_as_env()
 
 
-def update_dataproduct():
-    df = make_df()
-    append_to_bq(df)
+con = pesys_utils.open_pen_connection()
+df_kravstatus = pesys_utils.pandas_from_sql("../sql/kravstatus.sql", con)
+con.close()
+df_kravstatus.columns = map(str.lower, df_kravstatus.columns)
+df_kravstatus["dato"] = datetime.now()
 
+table_id = f"pensjon-saksbehandli-prod-1f83.saksstatistikk.kravstatus"
+job_config = LoadJobConfig(
+    schema=[
+        SchemaField("sakstype", enums.SqlTypeNames.STRING),
+        SchemaField("kravtype", enums.SqlTypeNames.STRING),
+        SchemaField("kravstatus", enums.SqlTypeNames.STRING),
+        SchemaField("antall", enums.SqlTypeNames.INTEGER),
+        SchemaField("dato", enums.SqlTypeNames.TIMESTAMP),
+    ],
+    write_disposition="WRITE_APPEND",
+)
 
-def make_df():
-    con = pesys_utils.open_pen_connection()
-    df_kravstatus = pesys_utils.pandas_from_sql('../sql/kravstatus.sql', con)
-    con.close()
-    df_kravstatus.columns = map(str.lower, df_kravstatus.columns)
-    df_kravstatus["dato"] = datetime.utcnow()
-    return df_kravstatus
+client = Client(project="pensjon-saksbehandli-prod-1f83")
 
-
-def append_to_bq(df):
-    table_id = f'pensjon-saksbehandli-prod-1f83.saksstatistikk.kravstatus'
-    job_config = LoadJobConfig(
-        schema = [
-            SchemaField("sakstype", enums.SqlTypeNames.STRING),
-            SchemaField("kravtype", enums.SqlTypeNames.STRING),
-            SchemaField("kravstatus", enums.SqlTypeNames.STRING),
-            SchemaField("antall", enums.SqlTypeNames.INTEGER),
-            SchemaField("dato", enums.SqlTypeNames.TIMESTAMP),
-        ],
-        write_disposition="WRITE_APPEND",
-    )
-
-    client = Client(project="pensjon-saksbehandli-prod-1f83")
-
-    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
-    job.result()
-    print(f"Table {table_id} successfully updated")
-
-
-if __name__ == "__main__":
-    update_dataproduct()
+job = client.load_table_from_dataframe(df_kravstatus, table_id, job_config=job_config)
+job.result()
+print(f"Table {table_id} successfully updated")

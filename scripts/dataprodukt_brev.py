083874463b7a02ea9e9fd590e792d1476b4c700f
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-03-18 14:44:46 +0100

    nytt dataprodukt med innsikt på inntektsendringsbrev

---
+++ scripts/dataprodukt_brev.py
@@ -0,0 +1,34 @@
+import logging
+from google.cloud.bigquery import Client, LoadJobConfig
+
+from lib import pesys_utils
+
+logging.basicConfig(level=logging.INFO)
+pesys_utils.set_pen_secrets_as_env()
+
+# oracle
+tuning = 10000
+con = pesys_utils.open_pen_connection()
+df_autobrev_inntektsendring = pesys_utils.pandas_from_sql(
+    sql_file="../sql/autobrev_inntektsendring.sql",
+    con=con,
+    tuning=tuning,
+    lowercase=True,
+)
+con.close()
+
+# bigquery
+client = Client(project="pensjon-saksbehandli-prod-1f83")
+job_config = LoadJobConfig(
+    write_disposition="WRITE_TRUNCATE",
+    create_disposition="CREATE_IF_NEEDED",
+)
+
+bq_datasett = "pensjon-saksbehandli-prod-1f83.brev"
+bq_autobrev_inntektsendring = f"{bq_datasett}.autobrev_inntektsendring"
+
+
+run_job = client.load_table_from_dataframe(
+    df_autobrev_inntektsendring, bq_autobrev_inntektsendring, job_config=job_config
+)
+run_job.result()
---
+++ sql/autobrev_inntektsendring.sql
@@ -0,0 +1,49 @@
+select
+    extract(year from kh.dato_onsket_virk) as ar,
+    extract(month from kh.dato_onsket_virk) as maned,
+    concat(
+        extract(year from kh.dato_onsket_virk), '-',
+        lpad(extract(month from kh.dato_onsket_virk), 2, '0')
+    ) as armaned,
+    (case
+        when substr(v.opprettet_av, 1, 1) in ('0','1','2','3','4','5','6','7','8','9')
+            then 'Bruker'
+        when
+            substr(v.opprettet_av, 1, 1) not in ('0','1','2','3','4','5','6','7','8','9')
+            and substr(v.opprettet_av, 2, 1) in ('0','1','2','3','4','5','6','7','8','9')
+            then 'Saksbehandler'
+        else v.opprettet_av
+    end) as opprettet_av,
+    (case when ab.k_batchbrev_id is not null then 'Autobrev' else 'Manuelt/ingen' end) as brevtype,
+    lower(kh.k_behandling_t) as behandlingstype,
+    count(1) as antall
+from pen.t_kravhode kh
+inner join pen.t_vedtak v
+    on
+        v.kravhode_id = kh.kravhode_id
+        and v.k_vedtak_t != 'REGULERING'
+        and v.dato_iverksatt is not null
+left outer join pen.t_autobrev ab
+    on ab.vedtak_id = v.vedtak_id
+where
+    kh.k_krav_gjelder = 'INNT_E' -- inntektsendring
+    and kh.k_krav_s = 'FERDIG'
+    and extract(year from kh.dato_onsket_virk) >= 2022
+group by
+    extract(year from kh.dato_onsket_virk),
+    extract(month from kh.dato_onsket_virk),
+    (case
+        when substr(v.opprettet_av, 1, 1) in ('0','1','2','3','4','5','6','7','8','9')
+            then 'Bruker'
+        when
+            substr(v.opprettet_av, 1, 1) not in ('0','1','2','3','4','5','6','7','8','9')
+            and substr(v.opprettet_av, 2, 1) in ('0','1','2','3','4','5','6','7','8','9')
+            then 'Saksbehandler'
+        else v.opprettet_av
+    end),
+    (case when ab.k_batchbrev_id is not null then 'Autobrev' else 'Manuelt/ingen' end),
+    kh.k_behandling_t
+order by
+    extract(year from kh.dato_onsket_virk) desc,
+    extract(month from kh.dato_onsket_virk) desc,
+    count(1) desc

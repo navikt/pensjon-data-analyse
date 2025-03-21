c393255229c80e8bef2c6f639fdffe821ea939fd
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-03-21 13:59:37 +0100

    bugfix på nytt filnavn

--- dags/aldersovergang.py
+++ dags/aldersovergang.py
@@ -3,13 +3,13 @@ from airflow.utils.dates import days_ago
 from dataverk_airflow import python_operator
 
 
-with DAG('dataproduct-aldersovergang', start_date=days_ago(1), schedule_interval="0 0 1-7 * 4", catchup=False) as dag:
+with DAG('aldersovergang', start_date=days_ago(1), schedule_interval="0 0 1-7 * 4", catchup=False) as dag:
     t1 = python_operator(
         dag=dag,
         name="dataproduct-aldersovergang",
         # slack_channel="#pensak-airflow-alerts",
         repo="navikt/pensjon-data-analyse",
-        script_path="scripts/dataproduct_aldersovergang.py",
+        script_path="scripts/aldersovergang.py",
         requirements_path="requirements.txt",
         use_uv_pip_install=True,
         allowlist=["secretmanager.googleapis.com", "bigquery.googleapis.com", "dm09-scan.adeo.no:1521"]

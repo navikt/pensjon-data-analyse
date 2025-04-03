22904b0f08065980297b2ee864e8721d8a349ece
Brynjar <141220683+navbrynjar@users.noreply.github.com>
2025-04-03 10:00:53 +0200

    tue funket ikke i cron, prøver med TUE

--- dags/aldersovergang.py
+++ dags/aldersovergang.py
@@ -3,7 +3,7 @@ from airflow.utils.dates import days_ago
 from dataverk_airflow import python_operator
 
 
-with DAG('aldersovergang', start_date=days_ago(1), schedule_interval="0 0 2-8 * tue", catchup=False) as dag:
+with DAG('aldersovergang', start_date=days_ago(1), schedule_interval="0 1 2-8 * TUE", catchup=False) as dag:
     t1 = python_operator(
         dag=dag,
         name="dataproduct-aldersovergang",

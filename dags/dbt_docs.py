91cbd2d354eb4b274428f8733b8f1bb4f915be47
Eystein Waade <eystein.waade@nav.no>
2025-07-01 08:29:45 +0200

    ✨ Dag for å publisere dbt-dokumentasjon

---
+++ dags/dbt_docs.py
@@ -0,0 +1,35 @@
+import pendulum
+from datetime import datetime
+from airflow import DAG
+from airflow.models import Variable
+
+from operators.dbt_operator import dbt_operator
+
+
+db_adresse = "dmv36db01.adeo.no:1521"
+docs_adresse = "dbt.intern.nav.no"
+
+
+with DAG(
+    dag_id="dbt_docs",
+    start_date=datetime(2025, 6, 30, tzinfo=pendulum.timezone("Europe/Oslo")),
+    schedule_interval="0 6 * * 1",  # “6 AM monday every week”
+    catchup=False,
+    depends_on_past=False,
+) as dag:
+    pen_dataprodukt = dbt_operator(
+        dag=dag,
+        name="pen_dataprodukt",
+        startup_timeout_seconds=60 * 10,
+        repo="navikt/pensjon-pen-dataprodukt",
+        script_path="dbt/dbt_run.py",
+        dbt_command="docs generate",
+        allowlist=[
+            "dmv36db01.adeo.no:1521",
+            "dbt.intern.nav.no",
+            "hub.getdbt.com",
+        ],
+        dbt_secret_name="pen-q2-pen_dataprodukt",
+    )
+
+    pen_dataprodukt

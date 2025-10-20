071f78cab03b607945856c035a613d362b984c00
Anders.Lunde <anders.lunde@nav.no>
2025-10-20 08:37:26 +0200

    Endrer kolonnenavn fra 'opprettet_maned' til 'kjoremaned'

--- sql/manedlig_omregning_manuelt.sql
+++ sql/manedlig_omregning_manuelt.sql
@@ -1,8 +1,8 @@
 --- manuell per ytelse per mnd
-select to_char(opp.dato_opprettet, 'yyyy-mm') as opprettet_maned,
+select to_char(opp.dato_opprettet, 'yyyy-mm') as kjoremaned,
        opp.ytelse,
        count(opp.manuell)                     as auto
 from pen.t_opptjeningsendring_bruker opp
 where opp.manuell = '1'
 group by to_char(opp.dato_opprettet, 'yyyy-mm'), opp.ytelse, opp.manuell
-order by opprettet_maned desc, opp.ytelse, manuell desc;
+order by kjoremaned desc, opp.ytelse, manuell desc;
--- sql/manedlig_omregning_okning.sql
+++ sql/manedlig_omregning_okning.sql
@@ -1,8 +1,8 @@
 --- okning per ytelse per mnd
-select to_char(opp.dato_opprettet, 'yyyy-mm') as opprettet_maned,
+select to_char(opp.dato_opprettet, 'yyyy-mm') as kjoremaned,
        opp.ytelse,
        count(opp.okning)                      as okning
 from pen.t_opptjeningsendring_bruker opp
 where opp.okning = '1'
 group by to_char(opp.dato_opprettet, 'yyyy-mm'), opp.ytelse, opp.okning
-order by opprettet_maned desc, opp.ytelse, okning desc;
+order by kjoremaned desc, opp.ytelse, okning desc;

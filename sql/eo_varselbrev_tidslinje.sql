8b0b84318185e1b07cc77020390b0bc884ab4f9e
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-10-21 14:05:14 +0200

    🎨 sql-formattering

--- sql/eo_varselbrev_tidslinje.sql
+++ sql/eo_varselbrev_tidslinje.sql
@@ -8,33 +8,34 @@
 select
     extract(year from dato_onsket_virk) as eo_ar,
     case
-        when kh.dato_mottatt_krav in ( -- hovedkjøringene
-        to_date('21/10/25', 'DD/MM/YY'),
-        to_date('22/10/24', 'DD/MM/YY'),
-        to_date('24/10/23', 'DD/MM/YY'),
-        to_date('18/10/22', 'DD/MM/YY'),
-        to_date('19/10/21', 'DD/MM/YY'),
-        to_date('21/10/20', 'DD/MM/YY')
-    ) then 'Hovedkjøring'
+        when dato_mottatt_krav in ( -- hovedkjøringene
+            to_date('21/10/25', 'DD/MM/YY'),
+            to_date('22/10/24', 'DD/MM/YY'),
+            to_date('24/10/23', 'DD/MM/YY'),
+            to_date('18/10/22', 'DD/MM/YY'),
+            to_date('19/10/21', 'DD/MM/YY'),
+            to_date('21/10/20', 'DD/MM/YY')
+        ) then 'Hovedkjøring'
         else 'Ikke hovedkjøring'
     end as hovedkjoring,
     trunc(dato_opprettet) as dato_opprettet,
     count(*) as antall_krav_ut_vurdering_eo
-from pen.t_kravhode kh
-where 1=1
-    and kh.k_krav_gjelder = 'UT_VURDERING_EO' -- kun ett varselbrev per sakid per år
+from pen.t_kravhode
+where
+    1 = 1
+    and k_krav_gjelder = 'UT_VURDERING_EO' -- kun ett varselbrev per sakid per år
     and extract(year from dato_onsket_virk) >= 2019
 group by
     extract(year from dato_onsket_virk),
     case
-        when kh.dato_mottatt_krav in ( -- hovedkjøringene
-        to_date('21/10/25', 'DD/MM/YY'),
-        to_date('22/10/24', 'DD/MM/YY'),
-        to_date('24/10/23', 'DD/MM/YY'),
-        to_date('18/10/22', 'DD/MM/YY'),
-        to_date('19/10/21', 'DD/MM/YY'),
-        to_date('21/10/20', 'DD/MM/YY')
-    ) then 'Hovedkjøring'
+        when dato_mottatt_krav in ( -- hovedkjøringene
+            to_date('21/10/25', 'DD/MM/YY'),
+            to_date('22/10/24', 'DD/MM/YY'),
+            to_date('24/10/23', 'DD/MM/YY'),
+            to_date('18/10/22', 'DD/MM/YY'),
+            to_date('19/10/21', 'DD/MM/YY'),
+            to_date('21/10/20', 'DD/MM/YY')
+        ) then 'Hovedkjøring'
         else 'Ikke hovedkjøring'
     end,
     trunc(dato_opprettet)

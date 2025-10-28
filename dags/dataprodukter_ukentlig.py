41ec21aa2070f7c3705825799cc4c1c4d98900c0
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-10-28 14:23:45 +0000

    📈 script for feilutbetalinger og ny DAG for ukentlige dataprodukter

---
+++ dags/dataprodukter_ukentlig.py
@@ -0,0 +1,51 @@
+from airflow import DAG
+from datetime import datetime
+from pendulum import timezone
+from kubernetes import client as k8s
+from dataverk_airflow import python_operator
+
+# DAG for ukentlig oppdaterte dataprodukter
+# Kan være data som ikke trenger å oppdateres daglig
+# eller spørringer som tar litt tid, eller begge
+
+
+def python_operator_wrapped(
+    *,  # Enforce keyword-only arguments
+    dag: DAG,
+    name: str,
+    script_path: str,
+    resources: k8s.V1ResourceRequirements = None,
+):
+    """Wrapper dataverk_airflow.python_operator with default arguments."""
+    return python_operator(
+        dag=dag,
+        name=name,
+        script_path=script_path,
+        requirements_path="requirements.txt",
+        use_uv_pip_install=True,
+        repo="navikt/pensjon-data-analyse",
+        slack_channel="#pensak-airflow-alerts",
+        allowlist=[
+            "secretmanager.googleapis.com",
+            "bigquery.googleapis.com",
+            "dmv14-scan.adeo.no:1521",  # prod lesekopien
+        ],
+        resources=resources,
+        python_version="3.12",
+    )
+
+
+with DAG(
+    dag_id="dataprodukter_ukentlig",
+    description="Ukentlig oppdatering av dataprodukter, altså BQ-tabeller",
+    schedule_interval="15 5 * * sun",
+    start_date=datetime(2025, 3, 12, tzinfo=timezone("Europe/Oslo")),
+    catchup=False,
+) as dag:
+    feilutbetalinger = python_operator_wrapped(
+        dag=dag,
+        name="feilutbetalinger",
+        script_path="scripts/feilutbetalinger.py",
+    )
+
+    feilutbetalinger
---
+++ scripts/feilutbetalinger.py
@@ -0,0 +1,47 @@
+import sys
+import logging
+from pathlib import Path
+from google.cloud.bigquery import LoadJobConfig
+sys.path.append(str(Path(__file__).parent.parent / "libs"))
+from utils import pesys_utils, gcp_utils
+
+logging.basicConfig(level=logging.INFO)
+
+# Metabase-dashbord for feilutbetalinger, se https://metabase.ansatt.nav.no/dashboard/903-feilutbetalinger-alderspensjon
+bq_feilutbetalinger = "pensjon-saksbehandli-prod-1f83.feilutbetalinger.feilutbetalinger"
+bq_feilutbetalinger_dodsalder = "pensjon-saksbehandli-prod-1f83.feilutbetalinger.feilutbetalinger_dodsalder"
+bq_dodsfall_alderspensjon = "pensjon-saksbehandli-prod-1f83.feilutbetalinger.dodsfall_alderspensjon"
+
+# oracle PEN
+tuning = 10000
+pesys_utils.set_db_secrets(secret_name="pen-prod-lesekopien-pen_dataprodukt")
+con = pesys_utils.connect_to_oracle()
+df_feilutbetalinger = pesys_utils.pandas_from_sql(
+    "../sql/feilutbetalinger_alderspensjonister/feilutbetalinger.sql", con=con, tuning=tuning, lowercase=True
+)
+df_feilutbetalinger_dodsalder = pesys_utils.pandas_from_sql(
+    "../sql/feilutbetalinger_alderspensjonister/feilutbetalinger_dodsalder.sql", con=con, tuning=tuning, lowercase=True
+)
+df_dodsfall_alderspensjon = pesys_utils.pandas_from_sql(
+    "../sql/feilutbetalinger_alderspensjonister/dodsfall_alderspensjon.sql", con=con, tuning=tuning, lowercase=True
+)
+con.close()
+
+
+client = gcp_utils.get_bigquery_client(
+    project="pensjon-saksbehandli-prod-1f83", target_principal="bq-airflow@wendelboe-prod-801c.iam.gserviceaccount.com"
+)
+job_config = LoadJobConfig(
+    write_disposition="WRITE_TRUNCATE",
+    create_disposition="CREATE_IF_NEEDED",
+)
+
+
+job1 = client.load_table_from_dataframe(df_feilutbetalinger, bq_feilutbetalinger, job_config=job_config)
+job1.result()
+
+job2 = client.load_table_from_dataframe(df_feilutbetalinger_dodsalder, bq_feilutbetalinger_dodsalder, job_config=job_config)
+job2.result()
+
+job3 = client.load_table_from_dataframe(df_dodsfall_alderspensjon, bq_dodsfall_alderspensjon, job_config=job_config)
+job3.result()

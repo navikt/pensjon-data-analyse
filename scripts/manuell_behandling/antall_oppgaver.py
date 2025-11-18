8b96d045c33dbc8ca51056a551f64fe09b9619ec
Anders.Lunde <anders.lunde@nav.no>
2025-11-18 13:25:20 +0100

    Revert forrige endring i autobrev_inntektsendring.py og fix path issue i antall_oppgaver.py

--- scripts/manuell_behandling/antall_oppgaver.py
+++ scripts/manuell_behandling/antall_oppgaver.py
@@ -4,7 +4,7 @@ from google.cloud.bigquery import LoadJobConfig
 
 from scripts.manuell_behandling.underkategori_mapper import map_underkategori_kode
 
-sys.path.append(str(Path(__file__).parent.parent / "libs"))
+sys.path.append(str(Path(__file__).parent.parent.parent / "libs"))
 from utils import pesys_utils, gcp_utils
 
 # OBS! Dette kjøres som en append i BQ, så ved dobbeltkjøring vil det bli duplikater
@@ -16,7 +16,7 @@ pesys_utils.set_db_secrets(secret_name="pen-prod-lesekopien-pen_dataprodukt")
 con = pesys_utils.connect_to_oracle()
 
 df_antall_oppgaver = pesys_utils.pandas_from_sql(
-    "../sql/manuell_behandling_antall_oppgaver.sql", con=con, tuning=tuning, lowercase=True
+    "../../sql/manuell_behandling_antall_oppgaver.sql", con=con, tuning=tuning, lowercase=True
 )
 con.close()
 

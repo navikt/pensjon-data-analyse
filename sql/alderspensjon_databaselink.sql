db40d83c205f5fdbb0d11eccdbc269ede0b6fe26
Eystein Waade <eystein.waade@nav.no>
2025-12-08 09:07:41 +0100

    ♻️ restrukturer databaselink kode for PEN -> dvh dataprodukter  Co-authored-by: Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>

--- sql/alderspensjon_databaselink.sql
+++ sql/alderspensjon_databaselink.sql
@@ -2,22 +2,30 @@ CREATE DATABASE LINK "lesekopien_til_dvh_prod"
 CONNECT TO "" IDENTIFIED BY ""
 USING '(DESCRIPTION = (ADDRESS= (PROTOCOL=TCP) (HOST=dmv14-scan.adeo.no) (PORT=1521)) (CONNECT_DATA= (SERVICE_NAME=PEN_LES)))';
 
+DECLARE
+    periode_var varchar := '202511';
+BEGIN
 
-
-create table dataprodukt_alderspensjon_vedtak as 
-select * from pen_dataprodukt.dataprodukt_alderspensjon_vedtak@lesekopien_til_dvh_prod;
+--create table dataprodukt_alderspensjon_vedtak as 
+insert into dataprodukt_alderspensjon_vedtak
+select * from pen_dataprodukt.dataprodukt_alderspensjon_vedtak@lesekopien_til_dvh_prod
+where periode = periode_var;
 
 --drop table dataprodukt_alderspensjon_vedtak purge;
 
 
-create table dataprodukt_alderspensjon_beregning as 
-select * from pen_dataprodukt.dataprodukt_alderspensjon_beregning@lesekopien_til_dvh_prod;
+--create table dataprodukt_alderspensjon_beregning as 
+insert into dataprodukt_alderspensjon_beregning 
+select * from pen_dataprodukt.dataprodukt_alderspensjon_beregning@lesekopien_til_dvh_prod
+where periode = periode_var;
 
 --drop table dataprodukt_alderspensjon_belop purge;
 
 
-create table dataprodukt_alderspensjon_belop as 
-select * from pen_dataprodukt.dataprodukt_alderspensjon_belop@lesekopien_til_dvh_prod;
+--create table dataprodukt_alderspensjon_belop as 
+insert into dataprodukt_alderspensjon_belop 
+select * from pen_dataprodukt.dataprodukt_alderspensjon_belop@lesekopien_til_dvh_prod
+where periode = periode_var;
 
 --drop table dataprodukt_alderspensjon_belop purge;
 
@@ -26,7 +34,8 @@ select 1 from dual;
 
 drop database link lesekopien_til_dvh_prod;
 
-
+end;
+/
 
 -- Sett inn kode for views 
 -- create view wendelboe_pesys_alder_I_current

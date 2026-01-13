ad50d48360b67304122e2391205f59d2ecd4b5d4
Eystein Waade <eystein.waade@nav.no>
2026-01-13 09:29:23 +0100

    ♻️ skriv om try-except logikk
    Co-authored-by: Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>

--- scripts/dvh_sak_ufore.py
+++ scripts/dvh_sak_ufore.py
@@ -20,20 +20,23 @@ pesys_utils.set_db_secrets(secret_name="pen-q2-pen_dataprodukt")
 client = gcp_utils.get_bigquery_client(
     project="pensjon-saksbehandli-prod-1f83", target_principal="bq-airflow@wendelboe-prod-801c.iam.gserviceaccount.com"
 )
+
+# Sjekk om tabellen finnes og hent maks teknisk_tid
 create_disposition = "CREATE_NEVER"  # endres til CREATE_IF_NEEDED under hvis tabellen ikke finnes
-sql_pen_dev = "select * from pen_dataprodukt.behandlingsstatistikk_meldinger"
-# prøver å finne maks kjoretidspunkt i BQ, og hvis det finnes legges det til som en where
+maks_kjoretidspunkt_bq = "1900-01-01 00:00:00"
 try:  # hvis tabellen finnes, hent alle nye rader
     query = f"select max(teknisk_tid) as maks_kjoretidspunkt_bq from `{dev_table_id}`"
     results = client.query(query).result()
     df = results.to_dataframe()
-    if not df.empty and pd.notnull(df.iloc[0]["maks_kjoretidspunkt_bq"]):
-        maks_kjoretidspunkt_bq = df.iloc[0]["maks_kjoretidspunkt_bq"]
-        sql_pen_dev += f" where teknisk_tid > to_date('{maks_kjoretidspunkt_bq}', 'YYYY-MM-DD HH24:MI:SS')"
-        logging.info(f"Maks kjoretidspunkt i BQ: {maks_kjoretidspunkt_bq}")
 except NotFound:
-    logging.info("Tabellen finnes ikke i BQ, så oppretter ny tabell med alle rader fra oracle.")
+    logging.info(f"Tabellen {dev_table_id} finnes ikke i BQ, så oppretter ny tabell med alle rader fra oracle.")
     create_disposition = "CREATE_IF_NEEDED"
+    df = pd.DataFrame()
+
+if not df.empty and pd.notnull(df.iloc[0]["maks_kjoretidspunkt_bq"]):
+    maks_kjoretidspunkt_bq = df.iloc[0]["maks_kjoretidspunkt_bq"]
+    logging.info(f"Maks kjoretidspunkt i BQ: {maks_kjoretidspunkt_bq}")
+sql_pen_dev = f"select * from pen_dataprodukt.behandlingsstatistikk_meldinger where teknisk_tid > to_date('{maks_kjoretidspunkt_bq}', 'YYYY-MM-DD HH24:MI:SS')"
 
 con = pesys_utils.connect_to_oracle()
 df_bq = pesys_utils.df_from_sql(sql_pen_dev, con)

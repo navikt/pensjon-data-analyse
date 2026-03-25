636293ac0abad4737d0eba919dd2f20c0e2c103b
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2026-03-25 14:58:01 +0100

    ✨ Datalast av stønad alder fra oracle til BQ
    Co-authored-by: Eystein Waade <eystein.waade@nav.no>

---
+++ dags/oracle_til_bq_stonad.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from datetime import datetime
+from pendulum import timezone
+from dataverk_airflow import python_operator
+
+with DAG(
+    dag_id="oracle_til_bq_stonad",
+    description="Overføring av dataprodukter fra oracle til BQ for stønadsstatistikken til Team Pensjon DVH",
+    start_date=datetime(2026, 3, 24, tzinfo=timezone("Europe/Oslo")),
+    schedule_interval="0 4 * * *",  # 4:00 hver dag
+    catchup=False,
+) as dag:
+    datalast_stonad_alder_q2 = python_operator(
+        dag=dag,
+        name="datalast_stonad_alder_q2",
+        script_path="scripts/dvh_stonad_alder.py",
+        requirements_path="requirements.txt",
+        use_uv_pip_install=True,
+        repo="navikt/pensjon-data-analyse",
+        slack_channel="#pensak-airflow-alerts",
+        allowlist=[
+            "secretmanager.googleapis.com",
+            "bigquery.googleapis.com",
+            "dmv36-scan.adeo.no:1521",  # q2
+        ],
+        python_version="3.12",
+        extra_envs={"ENVIRONMENT": "dev"},  # eller "prod"
+    )
+
+    datalast_stonad_alder_prod = python_operator(
+        dag=dag,
+        name="datalast_stonad_alder_prod",
+        script_path="scripts/dvh_stonad_alder.py",
+        requirements_path="requirements.txt",
+        use_uv_pip_install=True,
+        repo="navikt/pensjon-data-analyse",
+        slack_channel="#pensak-airflow-alerts",
+        allowlist=[
+            "secretmanager.googleapis.com",
+            "bigquery.googleapis.com",
+            "dmv14-scan.adeo.no:1521",  # prod lesekopien
+        ],
+        python_version="3.12",
+        extra_envs={"ENVIRONMENT": "prod"},  # eller "prod"
+    )
+
+    datalast_stonad_alder_q2
+    datalast_stonad_alder_prod
---
+++ scripts/dvh_stonad_alder.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import logging
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent / "libs"))
+from utils import pesys_utils, gcp_utils
+from utils.oracle_to_bigquery import delta_load_oracle_table_to_bigquery, JobConfig
+
+logging.basicConfig(level=logging.INFO)
+
+
+ENVIRONMENT = "prod" if os.getenv("ENVIRONMENT") == "prod" else "dev"
+if ENVIRONMENT == "dev":
+    GCP_PROJECT_ID = "pensjon-saksbehandli-dev-cb76"
+    GCP_SECRET_NAME = "pen-q2-pen_dataprodukt"
+    TARGET_PRINCIPAL = "bq-airflow-dvh@pensjon-saksbehandli-dev-cb76.iam.gserviceaccount.com"
+elif ENVIRONMENT == "prod":
+    GCP_PROJECT_ID = "pensjon-saksbehandli-prod-1f83"
+    GCP_SECRET_NAME = "pen-prod-lesekopien-pen_dataprodukt"
+    TARGET_PRINCIPAL = "bq-airflow-dvh@pensjon-saksbehandli-prod-1f83.iam.gserviceaccount.com"
+else:
+    raise ValueError(f"Ukjent environment: {ENVIRONMENT}")
+DATASET_NAME = "stonadsstatistikk"
+TABLES = {
+    "stonadsstatistikk_alder_belop": "pen_dataprodukt.stonadsstatistikk_alder_belop",
+    "stonadsstatistikk_alder_beregning": "pen_dataprodukt.stonadsstatistikk_alder_beregning",
+    "stonadsstatistikk_alder_vedtak": "pen_dataprodukt.stonadsstatistikk_alder_vedtak",
+}
+
+if __name__ == "__main__":
+    client = gcp_utils.get_bigquery_client(project=GCP_PROJECT_ID, target_principal=TARGET_PRINCIPAL)
+    pesys_utils.set_db_secrets(secret_name=GCP_SECRET_NAME)
+    oracle_client = pesys_utils.connect_to_oracle()
+    for ORACLE_TABLE, TABLE_NAME in TABLES.items():
+        job_config = JobConfig(
+            oracle_table=ORACLE_TABLE,
+            bigquery_table=TABLE_NAME,
+            gcp_project=GCP_PROJECT_ID,
+            bigquery_dataset_name=DATASET_NAME,
+            delta_column_name_oracle="kjoretidspunkt",
+            delta_column_name_bigquery="kjoretidspunkt",
+            bigquery_schema=None,  # type: ignore
+        )
+        delta_load_oracle_table_to_bigquery(
+            oracle_client=oracle_client,
+            bigquery_client=client,
+            job_config=job_config,
+        )
+        logging.info(f"Ferdig med tabellen {TABLE_NAME}.")
+    logging.info("Fullført overføring av data fra Oracle til BigQuery.")

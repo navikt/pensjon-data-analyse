f23d2af0264ed7d6281b1c9f2e4c438b80d9ea90
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2026-04-20 16:22:14 +0200

    🐛 Fikser feil sørvisbruker
    Co-authored-by: Eystein Waade <eystein.waade@nav.no>

--- scripts/ufore_diagnosekoder.py
+++ scripts/ufore_diagnosekoder.py
@@ -18,7 +18,7 @@ df_diagnosekoder = pesys_utils.df_from_sql(
 con.close()
 
 client = gcp_utils.get_bigquery_client(
-    project="pensjon-saksbehandli-prod-1f83", target_principal="bq-airflow@wendelboe-prod-801c.iam.gserviceaccount.com"
+    project="pensjon-saksbehandli-prod-1f83", target_principal="bq-airflow-dvh@pensjon-saksbehandli-prod-1f83.iam.gserviceaccount.com"
 )
 job_config = LoadJobConfig(
     write_disposition="WRITE_TRUNCATE",

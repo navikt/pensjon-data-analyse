8e634bfd6870b77bdce76cf8a284418ca74e79d9
Eystein Waade <eystein.waade@nav.no>
2026-04-27 12:52:44 +0200

    ♻️ dataprodukt diagnosekoder til pen_dataprodukt_dataset
    Co-authored-by: Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>

--- scripts/ufore_diagnosekoder.py
+++ scripts/ufore_diagnosekoder.py
@@ -6,7 +6,7 @@ sys.path.append(str(Path(__file__).parent.parent / "libs"))
 from utils import pesys_utils, gcp_utils
 logging.basicConfig(level=logging.INFO)
 
-bq_target = "pensjon-saksbehandli-prod-1f83.stonadsstatistikk.dataprodukt_ufore_diagnosekoder"
+bq_target = "pensjon-saksbehandli-prod-1f83.pen_dataprodukt_dataset.dataprodukt_ufore_diagnosekoder"
 
 pesys_utils.set_db_secrets(secret_name="pen-prod-lesekopien-pen_dataprodukt")
 con = pesys_utils.connect_to_oracle()
@@ -18,11 +18,10 @@ df_diagnosekoder = pesys_utils.df_from_sql(
 con.close()
 
 client = gcp_utils.get_bigquery_client(
-    project="pensjon-saksbehandli-prod-1f83", target_principal="bq-airflow-dvh@pensjon-saksbehandli-prod-1f83.iam.gserviceaccount.com"
+    project="pensjon-saksbehandli-prod-1f83", target_principal="bigqueryq-airflow-dvh@pensjon-saksbehandli-prod-1f83.iam.gserviceaccount.com"
 )
 job_config = LoadJobConfig(
-    write_disposition="WRITE_TRUNCATE",
-    create_disposition="CREATE_IF_NEEDED",
+    write_disposition="WRITE_TRUNCATE"
 )
 run_job = client.load_table_from_dataframe(df_diagnosekoder, bq_target, job_config=job_config)
 run_job.result()

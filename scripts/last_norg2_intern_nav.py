9f4ab114b69f60b5b11b35255ddfcd6f51666898
Eystein Waade <eystein.waade@nav.no>
2026-06-02 10:07:08 +0200

    🐛 skriv om jobb til å sette inn rå json

--- scripts/last_norg2_intern_nav.py
+++ scripts/last_norg2_intern_nav.py
@@ -1,50 +1,36 @@
 # API https://norg2.intern.nav.no/norg2/api/v1/enhet
 
-# Hent alle enheter og legg dem i en dictionary med orgnr som nøkkel
-# og enhetens navn som verdi. Skriv denne dictionaryen til en bigquery tabell.
+from pathlib import Path
+import sys
 
-import pandas as pd
 import requests
+sys.path.append(str(Path(__file__).parent.parent / "libs"))
+from utils import gcp_utils
 
-def load_org_enhet():
-    """
-    kolonner vi bryr os om: enhetNr, navn, enhetId, status
-    """
+from google.cloud import bigquery
+
+
+def fetch_raw_json() -> str:
     url = "https://norg2.intern.nav.no/norg2/api/v1/enhet"
     response = requests.get(url)
-    data: list[dict] = response.json()
+    response.raise_for_status()
+    return response.text
 
-    df = pd.DataFrame(data)
 
-    df = df[["enhetNr", "navn", "enhetId", "status"]]
-    # kun aktive enheter
-    df = df[df["status"] == "Aktiv"]
-
-    return df
-
-def load_single_org_enhet(enhetNr: str):
-    url = f"https://norg2.intern.nav.no/norg2/api/v1/enhet/{enhetNr}"
-    response = requests.get(url)
-    data: dict = response.json()
-    return data
-
-def insert_dim_org_enhet(df: pd.DataFrame, gcp_project: str, dataset: str, table: str):
-    from google.cloud import bigquery
-
-    client = bigquery.Client()
+def insert_raw_json(client: bigquery.Client, raw_json: str, gcp_project: str, dataset: str, table: str):
     table_id = f"{gcp_project}.{dataset}.{table}"
-    job_config = bigquery.LoadJobConfig(
-        write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
-    )
-    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
-    job.result()  # Wait for the job to complete.
-    print(f"Loaded {job.output_rows} rows into {table_id}.")
+    errors = client.insert_rows_json(table_id, [{"raw_json_payload": raw_json}])
+    if errors:
+        raise RuntimeError(f"Errors inserting rows: {errors}")
+    print(f"Inserted raw JSON into {table_id}.")
 
 
 if __name__ == "__main__":
-    # show stats about the data
-    df = load_org_enhet()
+    raw_json = fetch_raw_json()
+
     dataset_name = "raw_api_data"
     table_name = "norg2_intern_nav"
-    insert_dim_org_enhet(df, "pensjon-saksbehandli-dev-cb76", dataset_name, table_name) # dev
-    insert_dim_org_enhet(df, "pensjon-saksbehandli-prod-1f83", dataset_name, table_name) # prod
+    project = "pensjon-saksbehandli-prod-1f83"
+    target_principal = "bigquery-airflow-dvh@pensjon-saksbehandli-prod-1f83.iam.gserviceaccount.com"
+    client = gcp_utils.get_bigquery_client(project=project, target_principal=target_principal)
+    insert_raw_json(client, raw_json, project, dataset_name, table_name)

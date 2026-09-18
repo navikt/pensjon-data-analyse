6141c5e425bfc60b4f93dc9085d5170a6d5ec52e
Eystein Waade <eystein.waade@nav.no>
2026-09-18 11:00:54 +0200

    skriv til riktig tabell på bq
    Co-authored-by: Lise Nguyen <lise.nguyen@nav.no>

--- scripts/dvh_stonad_alder.py
+++ scripts/dvh_stonad_alder.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     # "styringsinfo_sak_uforetrygd": "pen_dataprodukt.styringsinfo_sak_uforetrygd"
     job_config = JobConfig(
         oracle_table="pen_dataprodukt.styringsinfo_sak_uforetrygd",
-        bigquery_table="styringsinfo_sak_uforetrygd",
+        bigquery_table="styringscockpit_sak_ufore",
         gcp_project=GCP_PROJECT_ID,
         bigquery_dataset_name=DATASET_NAME,
         delta_column_name_oracle="kjoretidspunkt",
